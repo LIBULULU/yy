ec56386b631328f1450debbfce31ad5fa3b58140
LIBULULU <15521220784@163.com>
2018-09-14 12:02:54 +0800

    demo

--- src/WC2/enter.java
+++ src/WC2/enter.java
@@ -9,12 +9,28 @@ import com.sun.javafx.sg.prism.web.NGWebView;
 
 public class enter {
 	public static void  main(String[] args) throws IOException{
-			System.out.println("请输入指令及路径");
+			System.out.println("请输入指令及路径:");
 			Scanner scanner = new Scanner(System.in); 
 			File file = new File(scanner.nextLine());
 		if(file.exists() == false) {
 			System.out.println("File does not exist");
-			return;
+			}
+		else {
+			concretefunctions c1 =new concretefunctions();
+			c1.getcodenum(file);
+			System.out.println("字符数："+c1.getcodenum(file));
+			
+			concretefunctions c2 =new concretefunctions();
+			c2.getLinenum(file);
+			System.out.println("行数："+c2.getLinenum(file));
+			
+			concretefunctions c3 =new concretefunctions();
+			c3.getwordnum(file);
+			System.out.println("单词数："+c3.getwordnum(file));
+			
+			
+			
+		}
 		}
 	}
-	}
+	
